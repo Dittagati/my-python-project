23eba32b695be8bcc16df5ee35eefcd28993fb4c
Dittagati <88185465+Dittagati@users.noreply.github.com>
2021-12-04 22:05:16 +0600

    night added

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import numpy as np
+arr_1 = np.array([1, 2, 3])
+arr_2 = np.array([2, 3, 4])
 
-matrix = np.array([1, 2, 3, 4, 5, 7, 8, 8])
-matrix_2 = np.array([[1, 2, 3], [1, 2, 3]])
-x = matrix.copy()
+adding = np.concatenate((arr_1, arr_2))
+print(adding)
 
-matrix[0] = 42
-
-print(x)
-print(matrix)
-print(matrix_2)
+matrix = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]]])
+for ndx, x in np.ndenumerate(matrix):
+    print(ndx, x)
 
