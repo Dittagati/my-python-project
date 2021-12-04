9bd4ea4e4b979d08601e669709147cba0c0d5f9e
Dittagati <88185465+Dittagati@users.noreply.github.com>
2021-12-04 23:41:54 +0600

    ädded

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
-arr_1 = np.array([1, 2, 3])
-arr_2 = np.array([2, 3, 4])
+arr_1 = np.array([[1, 2, 3], [4, 5, 6]])
+arr_2 = np.array([[2, 3, 4], [5, 6, 7]])
 
-adding = np.concatenate((arr_1, arr_2))
+adding = np.concatenate((arr_1, arr_2), axis=1)
 print(adding)
+
+add_2 = np.stack((arr_1, arr_2), axis=1)
+print(add_2)
 
 matrix = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]]])
 for ndx, x in np.ndenumerate(matrix):
@@ -14,3 +17,15 @@ y = 23
 
 z = x.__add__(y)
 print(z)
+
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+add_3 = np.hstack((x, y))
+print(add_3)
+
+add_4 = np.vstack((x, y))
+print(add_4)
+
+add_5 = np.dstack((x, y))
+print(add_5)
