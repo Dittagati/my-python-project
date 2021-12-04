6cc91a6040d302ad7b47c8a5530c98ea186197aa
Dittagati <88185465+Dittagati@users.noreply.github.com>
2021-12-04 10:41:56 +0600

    morning added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-string = np.array(['apple', 'coconut', 'banana'])
-print(string)
-print(string.dtype)
+matrix = np.array([1, 2, 3, 4, 5, 7, 8, 8])
 
-matrix = np.array([1.1, 2.0, 3.1, 4.6, 5.4, 3.3])
-new_array = matrix.astype('i')
-print(matrix.dtype)
-print(new_array)
-print(new_array.dtype)
+x = matrix.copy()
+
+matrix[0] = 42
+
+print(x)
+print(matrix)
+
+
