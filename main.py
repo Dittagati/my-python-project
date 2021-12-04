efce1cbf5ad3a0b89c7abe37a91a23b019c48350
Dittagati <88185465+Dittagati@users.noreply.github.com>
2021-12-04 10:43:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 matrix = np.array([1, 2, 3, 4, 5, 7, 8, 8])
-
+matrix_2 = np.array([[1, 2, 3], [1, 2, 3]])
 x = matrix.copy()
 
 matrix[0] = 42
 
 print(x)
 print(matrix)
-
+print(matrix_2)
 
